786ae72f46a2e68ec030148dac43b3ac8f26a058
gowthaman sel <gowthamanssel711@gmail.com>
2021-06-19 16:30:47 +0530

    Create obs.py

---
+++ launch/obs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import rospy 
+import numpy as np 
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist 
+
+
+def callback(data):
+    laser_data = np.array(data.ranges)
+    laser_data = laser_data[~np.isinf(laser_data)]
+
+    print(max(laser_data))
+
+    if max(laser_data) < 0.6:
+        take_turn()
+        move_forward()
+        laser_data=1
+    move_forward()
+
+def move_forward():
+    twist = Twist()
+    twist.linear.x = 0.1 
+    pub.publish(twist)
+    print("moving forward")
+
+def take_turn():
+    twist = Twist()
+    twist.angular.z = 1.57/2 # degree 180 / pi(90)
+    r = rospy.Rate(5.0)
+    for i in range(1):
+        pub.publish(twist)
+        r.sleep()
+
+def main():
+    sub = rospy.Subscriber("/scan", LaserScan, callback)
+    rospy.spin()
+
+rospy.init_node("Avoider")
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+
+if __name__ == '__main__':
+    main()
